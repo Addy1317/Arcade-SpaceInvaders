86d962e54748ee08f6acd866a8c903657e40bfa9
Aditya Singh <s.addy1317@gmail.com>
2024-05-25 17:31:35 +0530

    feat: Invader Movement - invaders Movement

---
+++ Assets/Scripts/Invader.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SlowpokeStudio.ArcadeSpaceInvaders
+{
+    public class Invader : MonoBehaviour
+    {
+        [Header("Sprites")]
+        [SerializeField] private Sprite[] _animateSprites;
+        [SerializeField] private float _animationTime = 1.0f;
+
+        private SpriteRenderer _spriteRenderer;
+        private int _animationFrame;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Start()
+        {
+            InvokeRepeating(nameof(AnimateSprite), this._animationTime, this._animationTime);
+        }
+
+        private void AnimateSprite()
+        {
+            _animationFrame++;
+
+            if (_animationFrame >= this._animateSprites.Length)
+            {
+                _animationFrame = 0;
+            }
+
+            _spriteRenderer.sprite = this._animateSprites[_animationFrame];
+        }
+    }
+}
--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -6,33 +6,79 @@ namespace SlowpokeStudio.ArcadeSpaceInvaders
 {
     public class Invaders : MonoBehaviour
     {
-        [Header("Sprites")]
-        [SerializeField] private Sprite[] _animateSprites;
-        [SerializeField] private float _animationTime = 1.0f;
+        [Header("Rows and Columns")]
+        [SerializeField] private int _rows;
+        [SerializeField] private int _columns;
 
-        private SpriteRenderer _spriteRenderer;
-        private int _animationFrame;
+        [Header("Move Speed")]
+        [SerializeField] private float _speed = 1.0f;
+
+        [Header("Invaders Prefab")]
+        [SerializeField] private Invader[] _invaderPrefabs;
+
+        private Vector3 _direction = Vector2.right;
 
         private void Awake()
         {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
+            InvadersSpawning();
         }
 
-        private void Start()
+        private void Update()
         {
-            InvokeRepeating(nameof(AnimateSprite), this._animationTime, this._animationTime);
+            InvadersMovement();
         }
 
-        private void AnimateSprite()
+        private void InvadersSpawning()
         {
-            _animationFrame++;
-
-            if (_animationFrame >= this._animateSprites.Length)
+            for (int row = 0; row < this._rows; row++)
             {
-                _animationFrame = 0;
-            }
+                float width = 2.0f * (this._columns - 1);
+                float height = 2.0f * (this._rows - 1);
+                Vector2 centering = new Vector2(-width / 2, -height / 2);
+                Vector3 rowPosition = new Vector3(centering.x, centering.y + (row * 2.0f), 0.0f);
 
-            _spriteRenderer.sprite = this._animateSprites[_animationFrame];
+                for (int col = 0; col < this._columns; col++)
+                {
+                    Invader invader = Instantiate(this._invaderPrefabs[row], this.transform);
+                    Vector3 position = rowPosition;
+                    position.x += col * 2.0f;
+                    invader.transform.localPosition = position;
+                }
+            }
+        }
+
+        private void InvadersMovement()
+        {
+            this.transform.position += _direction * this._speed * Time.deltaTime;
+
+            Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+            Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+            foreach (Transform invader in this.transform)
+            {
+                if (!invader.gameObject.activeInHierarchy)
+                {
+                    continue;
+                }
+
+                if(_direction == Vector3.right && invader.position.x >= (rightEdge.x - 1.0f))
+                {
+                    AdvanceRow();
+                }
+                else if(_direction == Vector3.left && invader.position.x <= (leftEdge.x + 1.0f))
+                {
+                    AdvanceRow();
+                }
+            }  
+        }
+
+        private void AdvanceRow()
+        {
+            _direction.x *= -1.0f;
+
+            Vector3 position = this.transform.position;
+            position.y -= 1.0f;
+            this.transform.position = position;
         }
     }
 }
